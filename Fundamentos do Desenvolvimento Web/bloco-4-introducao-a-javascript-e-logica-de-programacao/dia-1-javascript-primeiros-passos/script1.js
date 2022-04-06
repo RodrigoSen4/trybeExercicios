533b87e6c5d59f9da5a78aaf62ae6d6e36a4bfac
Rodrigo Sena <dias.rodrigosena@gmail.com>
2022-04-06 18:29:33 -0300

    Subindo exercicios dia 4.1

---
+++ Fundamentos do Desenvolvimento Web/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script1.js
@@ -0,0 +1,15 @@
+
+const a = 2;
+const b = 5;
+
+let Adicao = a + b;
+let Subtracao = b - a;
+let Multiplicacao = a * b;
+let Divisao = a / b;
+let Modulo = a % b;
+
+console.log(Subtracao);
+console.log(Adicao);
+console.log(Multiplicacao);
+console.log(Divisao);
+console.log(Modulo);
