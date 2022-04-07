264e00bef8f6618d28683d1f9a83644989642dd6
Rodrigo Sena <dias.rodrigosena@gmail.com>
2022-04-07 19:15:14 -0300

    Subindo exercicios

---
+++ Fundamentos do Desenvolvimento Web/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scriptEx7.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+const menorNumero = Math.min(...numbers);
+
+
+console.log(menorNumero);
+
+
+
+//Vi essa forma de resolver no site Hora de codar : https://www.horadecodar.com.br/2021/06/29/encontrar-maior-e-menor-numero-de-array-em-javascript/
